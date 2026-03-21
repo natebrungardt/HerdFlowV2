e69121e50b0a0e4643588396172d5973df7c30ad
Nate <natebrungardt15@gmail.com>
2026-03-20 20:36:14 -0500

    added create cow form connected to backend api

--- frontend/src/pages/CowsPage.tsx
+++ frontend/src/pages/CowsPage.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { getCows } from "../services/cowService";
+import { createCow, getCows } from "../services/cowService";
 import type { Cow } from "../types/cow";
 
 function CowsPage() {
   const [cows, setCows] = useState<Cow[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [tagNumber, setTagNumber] = useState("");
+  const [breed, setBreed] = useState("");
+  const [healthStatus, setHealthStatus] = useState("");
 
   useEffect(() => {
     async function loadCows() {
@@ -22,6 +25,25 @@ function CowsPage() {
     loadCows();
   }, []);
 
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const newCow = await createCow({
+        tagNumber,
+        breed,
+        healthStatus,
+      });
+
+      setCows((prev) => [...prev, newCow]);
+      setTagNumber("");
+      setBreed("");
+      setHealthStatus("");
+    } catch (err) {
+      setError("Failed to create cow");
+    }
+  }
+
   if (loading) return <p>Loading cows...</p>;
   if (error) return <p>{error}</p>;
 
@@ -30,6 +52,27 @@ function CowsPage() {
       <h1>HerdFlow</h1>
       <h2>All Cows</h2>
 
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Tag Number"
+          value={tagNumber}
+          onChange={(e) => setTagNumber(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Health Status"
+          value={healthStatus}
+          onChange={(e) => setHealthStatus(e.target.value)}
+        />
+        <button type="submit">Add Cow</button>
+      </form>
       {cows.length === 0 ? (
         <p>No cows found.</p>
       ) : (
--- frontend/src/services/cowService.ts
+++ frontend/src/services/cowService.ts
@@ -11,3 +11,29 @@ export async function getCows(): Promise<Cow[]> {
 
   return response.json();
 }
+
+export async function createCow(cowData: {
+  tagNumber: string;
+  breed: string;
+  healthStatus: string;
+}) {
+  const response = await fetch(API_BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      tagNumber: cowData.tagNumber,
+      breed: cowData.breed,
+      healthStatus: cowData.healthStatus,
+      heatStatus: "Not In Heat",
+      breedingStatus: "Open",
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create cow");
+  }
+
+  return response.json();
+}
